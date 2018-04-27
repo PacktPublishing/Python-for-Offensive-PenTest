fe7bcc0e1f68161d72b6535d36faac5be076a226
NirbhayaS <30429265+NirbhayaS@users.noreply.github.com>
2018-04-27 16:28:38 +0530

    Code Files Added

---
+++ Chapter06/Generate Keys.py
@@ -0,0 +1,21 @@
+# Python For Offensive PenTest
+
+# Download Pycrypto for Windows - pycrypto 2.6 for win32 py 2.7
+# http://www.voidspace.org.uk/python/modules.shtml#pycrypto
+
+# Download Pycrypto source
+# https://pypi.python.org/pypi/pycrypto
+# For Kali, after extract the tar file, invoke "python setup.py install"
+
+# Generate Keys
+
+from Crypto.PublicKey import RSA 
+new_key = RSA.generate(4096 ) # generate  RSA key that 4096 bits long
+
+#Export the Key in PEM format, the PEM extension contains ASCII encoding
+
+
+public_key = new_key.publickey().exportKey("PEM") 
+private_key = new_key.exportKey("PEM") 
+print private_key
+print public_key
